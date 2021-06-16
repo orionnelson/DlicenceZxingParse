1c08517ecba19817e3df7a0eac87ff0b17f961c1
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-06-16 17:31:50 -0400

    Bug Fix  Issues
    Fixed High Diffusion Countertops by implementing Rembg.
    Fixed Issues With card Rotation being Identified Incorrectly

--- barfind.py
+++ barfind.py
@@ -5,14 +5,19 @@ import imutils
 import cv2
 import matplotlib.pyplot as plt
 from scipy.spatial import distance as dist
-
+from rembg.bg import remove
+import io
+from PIL import Image
 debug = False
+import warnings
+warnings.filterwarnings("ignore")
 
 
 
 
 
 #Pre Image Processing Before Find Barcode is Used Taken From https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+# Future Use
 
 def order_points(pts):
 	# sort the points based on their x-coordinates
@@ -39,11 +44,53 @@ def order_points(pts):
 
 
 
+def four_point_transform(image, pts):
+	# obtain a consistent order of the points and unpack them
+	# individually
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+	# compute the width of the new image, which will be the
+	# maximum distance between bottom-right and bottom-left
+	# x-coordiates or the top-right and top-left x-coordinates
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+	# compute the height of the new image, which will be the
+	# maximum distance between the top-right and bottom-right
+	# y-coordinates or the top-left and bottom-left y-coordinates
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+	# now that we have the dimensions of the new image, construct
+	# the set of destination points to obtain a "birds eye view",
+	# (i.e. top-down view) of the image, again specifying points
+	# in the top-left, top-right, bottom-right, and bottom-left
+	# order
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+	# compute the perspective transform matrix and then apply it
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+	# return the warped image
+	return warped
+
+
+
 
 def cropreturn(image):
+    temp2 = 'temp2.png'
     ratio = image.shape[0] / 300.0
     orig = image.copy()
     image = imutils.resize(image, height = 300)
+    cv2.imwrite(temp2,image)
+    imgc = np.fromfile(temp2)
+    result = remove(imgc)
+    img = Image.open(io.BytesIO(result)).convert("RGBA")
+    img.save(temp2)
+    image = cv2.imread(temp2)
     # convert the image to grayscale, blur it, and find edges
     # in the image
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -80,13 +127,38 @@ def cropreturn(image):
                #print(rect)
     #print(screenCnt)
     if screenCnt is not None:
-            #print(lbox)
+            upright = True
+            #print(lbox[0])# Top Left 
+            #print(lbox[1])# Top Right
+            #print(lbox[2])# Bottom Right
+            #print(lbox[3])# Bottom Left
+            #print(lbox[0][0])
+            top = cv2.norm(lbox[0],lbox[1],cv2.NORM_L2)
+            #print(top)
+            rs = cv2.norm(lbox[1],lbox[2],cv2.NORM_L2)
+            sideways = top<rs
+            #print(rs)
+            temp = lbox[0]
+            if(sideways):
+                    dst_pts = np.array([[0, 0],   [1080, 0],  [1080, 1920], [0, 1920]], dtype=np.float32)
+            else:
+                    dst_pts = np.array([[0, 0],   [1920, 0],  [1920, 1080], [0, 1080]], dtype=np.float32)
+                    
+            #print(top)
+            #print(rs)
             #print("Lbox above")
             screenCnt = (lbox * ratio)
-            screenCnt = np.float32(screenCnt)
-            dst_pts = np.array([[0, 0],   [1920, 0],  [1920, 1080], [0, 1080]], dtype=np.float32)
+            screenCnt = np.float32(screenCnt)        
             M = cv2.getPerspectiveTransform(screenCnt, dst_pts)
-            warp = cv2.warpPerspective(orig, M, (1920, 1080))
+            # Card is Sideways
+            if(sideways):
+                    warp = cv2.warpPerspective(orig, M, (1080, 1920))
+                    if(lbox[0][1] > lbox[1][1]):
+                            warp = cv2.rotate(warp, cv2.ROTATE_90_CLOCKWISE)
+                    else:
+                            warp = cv2.rotate(warp, cv2.ROTATE_90)
+            else:
+                    warp = cv2.warpPerspective(orig, M, (1920, 1080))
             if debug: cv2.imshow("Card Found", warp)
             if debug: cv2.waitKey(0)
             if(warp is not None):
@@ -311,9 +383,3 @@ def findbarcode(image):
     output = thresh2
     if debug: cv2.waitKey(0)
     return output
-    
-
-
-
-
-
