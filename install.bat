b74aca0238e3dc9c9192c862c85c7862ce8d9c63
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-10-12 14:04:10 -0400

    Delete install.bat

--- install.bat
+++
@@ -1,4 +0,0 @@
-powershell -Command "& {python -m venv .}"
-powershell -Command "& {.\DlicenceZxingParse\Scripts\activate.bat}"
-powershell -Command "& {cd DlicenceZxingParse}"
-powershell -Command "& {.\ChocoMinGWCMakeInstaller.bat}"
